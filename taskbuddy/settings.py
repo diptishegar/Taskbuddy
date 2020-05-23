daa922e7418a41103e040e8ff8705e2ce1f3bc96
Dipti Shegar <diptishegar13@gmail.com>
2020-05-23 20:17:47 +0530

    shegar

--- taskbuddy/settings.py
+++ taskbuddy/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY='olh@&@5v9r_gxx+lj3&=x0*0^*2kr9-0c3tm3=%7*_e%&$(ns^'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['taskbro.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['taskbro.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
